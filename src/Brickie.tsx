337987e3272ba76faef775e99e41b69a6398f16c
Sandeep Gupta <sandy.pec@gmail.com>
2020-02-08 17:50:23 -0800

    allow multiple brickie to run on the same page

--- src/Brickie.tsx
+++ src/Brickie.tsx
@@ -10,13 +10,42 @@ import Bricks from './Bricks';
  */
 export default class Brickie {
 
-    private static reactElement: any;
-
-    private static domElement: HTMLElement;
-
+    /**
+     * An identifier counter to generate unique keys
+     */
     private static idCounter: number = 0;
 
-    static layout(json: object, mountElement: HTMLElement): void {
+    /**
+     * The react element that is currently rendered
+     */
+    private reactElement: any;
+
+    /**
+     * The HTML element on which the current UI is mounted
+     */
+    private domElement: HTMLElement;
+
+    /**
+     * Layout a given UI json. This convenience method creates a
+     * new instance of `Brickie` and lays out the UI, and then
+     * return the generated instance.
+     * 
+     * @param json 
+     * @param mountElement 
+     */
+    static lay(json: object, mountElement: HTMLElement): Brickie {
+        const brickie: Brickie = new Brickie();
+        brickie.lay(json, mountElement);
+        return brickie;
+    }
+
+    /**
+     * Lay out the UI using `BrickLayer`.
+     * 
+     * @param json 
+     * @param mountElement 
+     */
+    lay(json: object, mountElement: HTMLElement): void {
         if (!json) {
             throw new Error('JSON layout to render is a must.');
         }
@@ -29,24 +58,36 @@ export default class Brickie {
         Brickie.idCounter = 0;
         Brickie.addKeyField(json);
 
-        Brickie.domElement = mountElement;
-        Brickie.reactElement = ReactDOM.render(<BrickLayer layout={json} />, mountElement);
+        this.domElement = mountElement;
+        this.reactElement = ReactDOM.render(<BrickLayer layout={json} />, mountElement);
     }
 
-    static clear(): void {
-        if (!Brickie.domElement) {
+    /**
+     * Clear currently mounted components.
+     * 
+     */
+    clear(): void {
+        if (!this.domElement) {
             return;
         }
 
-        ReactDOM.unmountComponentAtNode(Brickie.domElement);
+        ReactDOM.unmountComponentAtNode(this.domElement);
 
         // clear up references
-        Brickie.domElement = null;
-        if (Brickie.reactElement) {
-            Brickie.reactElement = null;
+        this.domElement = null;
+        if (this.reactElement) {
+            this.reactElement = null;
         }
     }
 
+    /**
+     * Register bricks in the global context. A brick thus registered
+     * will be available for render immediately to all `Brickie`
+     * instances.
+     * 
+     * @param name 
+     * @param component 
+     */
     static registerBrick(name: string, component: Function): void {
         Bricks.registerBrick(name, component);
     }
@@ -60,21 +101,35 @@ export default class Brickie {
         Bricks.unregister(name);
     }
 
+    /**
+     * Unregister all registered bricks. After this call succeeds
+     * none of the `Brickie` instances will be able to render
+     * the UI.
+     * 
+     */
     static unregisterAll(): void {
         Bricks.unregisterAll();
     }
 
-    static addKeyField(json: any) {
+    /**
+     * Internal method to add `key` attributes to each brick
+     * in the JSON. This allows `React` to perform diffs faster.
+     * Any `key` attribute already existing on a brick is left
+     * as is.
+     * 
+     * @param json 
+     */
+    private static addKeyField(json: any) {
         if (!json) {
             return;
         }
-        
-        if(typeof json !== 'object') {
+
+        if (typeof json !== 'object') {
             return;
         }
 
-        if(Array.isArray(json)) {
-            for(let index:number = 0; index < json.length; index++) {
+        if (Array.isArray(json)) {
+            for (let index: number = 0; index < json.length; index++) {
                 const item = json[index];
                 Brickie.addKeyField(item);
             }
